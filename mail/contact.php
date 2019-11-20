98775ea27bab15d32395b23f8742322934348ec2
Jordan Ruckle <jordanruckle@yahoo.com>
2019-11-20 13:34:15 -0700

    Updated contact form email recipient

--- mail/contact.php
+++ mail/contact.php
@@ -34,7 +34,7 @@ if(get_magic_quotes_gpc()) {
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
-$address = "jordanruckle@yahoo.com";
+$address = "libby@thetablecasper.org";
 
 
 // Configuration option.
